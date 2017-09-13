e08231751d14c0f6d04b7c32d221fe0f2e77df8a
Riley Grigg <griggriley@gmail.com>
2017-09-13 08:21:40 -0600

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,28 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
 int main()
 {
+    cout << setfill ('.');
+    cout << "Riley Grigg, Senior, 2017-2018 \n \n";
+    cout << "A Day:\n";
+    cout << "A1" << setw (50);
+    cout << "Chemistry\n";
+    cout << "A2" << setw (50);
+    cout << "Peer Tutor\n";
+    cout << "A3" << setw (50);
+    cout << "Math 1010 CE\n";
+    cout << "A4" << setw (50);
+    cout << "Gov and Cit\n";
+    cout << "\n";
+    cout << "B Day:\n";
+    cout << "B1 \t \t \t \t";
+    cout << "Computer Programming 1\n";
+    cout << "B2 \t \t \t \t";
+    cout << "Psychology\n";
+    cout << "B3 \t \t \t \t";
+    cout << "Seminary\n";
+    cout << "B4 \t \t \t \t";
+    cout << "ASL 2";
 	return 0;
 }
